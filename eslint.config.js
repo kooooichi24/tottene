6659ae0475ac273feb41b09e040b827bcd2fbd3e
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-13 21:59:38 +0900

    chore: Remove Prettier recommended settings from ESLint configuration and clean up unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const { defineConfig } = require("eslint/config");
 
 module.exports = defineConfig([
   expoConfig,
-  eslintPluginPrettierRecommended,
-  {
-    ignores: ['dist/*'],
-  },
 ]);
